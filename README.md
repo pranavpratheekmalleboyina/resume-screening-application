8aec49627a229d2963efe4e43ddc569232365fea
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2024-12-05 12:16:42 -0600

    Prediction part completed in both notebook and server-side,README.md updated to include execution steps

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # Resume Screening application 
 
 ## Project 
-This project is about the prediction of the industry of the person whose resume would be uploaded in the application
+This project is about the prediction of the industry of the person whose resume would be uploaded in the application.
 
 ## Installation 
 
  **Dependencies:**
  install the python streamlit library by executing the following command:
- ```python
+ ```bash
 pip install streamlit
  ```
  Find out where you have installed your python scripts and place your executable `streamlit` in the same folder
+
+ Open the bash from the folder where the `app.py` file is located and run the following command to start the application.
+ ```bash
+streamlit run app.py
+ ```
 
 ## Project Structure
 
--- app.py
+++ app.py
@@ -7,14 +7,20 @@ import nltk
 nltk.download("punkt")
 nltk.download("stopwords")
 
+#loading the models
+tfidf = pickle.load(open('tfidf.pkl','rb'))
+clf = pickle.load(open('clf.pkl','rb'))
+
 def cleanup(text):
-    cleaned_text = re.sub('http\S+s*', ' ', text)
-    cleaned_text = re.sub('RT|cc', ' ', cleaned_text)
-    cleaned_text = re.sub('#\S*', ' ', cleaned_text)
-    cleaned_text = re.sub('@\S*', ' ', cleaned_text)
-    cleaned_text = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', cleaned_text)
-    cleaned_text = re.sub('\s+', ' ', cleaned_text)
-    cleaned_text = re.sub(r'[\x00-\x7f]', r' ', cleaned_text)
+    cleaned_text = re.sub(r'http\S+\s', ' ', text)
+    cleaned_text = re.sub(r'RT|cc', ' ', cleaned_text)
+    cleaned_text = re.sub(r'#\S+\s', ' ', cleaned_text)
+    cleaned_text = re.sub(r'@\S+', ' ', cleaned_text)
+    special_characters = re.escape(r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~""")
+    pattern = rf"[{special_characters}]"
+    cleaned_text = re.sub(pattern, ' ', cleaned_text)
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
+    cleaned_text = re.sub('[^\x00-\x7f]', ' ', cleaned_text)
     return cleaned_text
 
 #web app
@@ -28,8 +34,43 @@ def main():
            resume_text = resume_bytes.decode('utf-8')
        except UnicodeDecodeError:    
            resume_text = resume_bytes.decode('latin1')
-       cleaned_resume = cleanup([resume_text])      
-   
+       #st.write(resume_text)    
+       cleaned_resume = cleanup(resume_text)     
+       input_features = tfidf.transform([cleaned_resume]) 
+       prediction_id = clf.predict(input_features)[0]
+       
+       category_mapping = {
+        15: "Java Developer",
+        23: "Testing",
+        8 : "Devops Engineer",
+        20 : "Python Developer",
+        24: "Web Designer",
+        12 : "HR",
+        13: "Hadoop",
+        3 : "Blockchain",
+        10:"ETL Developer",
+        18: "Operations Manager",
+        6 : "Data Science",
+        22: "Sales",
+        16: "Mechanical Engineer",
+        1:"Art",
+        7:"Database",
+        8:"Electric Engineer",
+        14:"Health and Fitness",
+        19:"PMO",
+        4:"Business Analyst",
+        9:"dotnet developer",
+        2:"Automation tester",
+        17:"Network Security Engineer",
+        21:"SAP Developer",
+        5:"Civil Engineer",
+        0:"Advocate"
+ }
+
+       category_name = category_mapping.get(prediction_id,"unknown")
+       st.write(f"The category is {category_name}")
+       #st.write(f"The prediction id is {prediction_id}") 
+       
 if __name__ == "__main__":
     main()   
 
